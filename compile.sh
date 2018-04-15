03c5b2725f6688e44e27daae7273eba0984b64bf
michele <michele.voto@gmail.com>
2018-04-15 17:40:18 +0200

    Modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,17 +1,3 @@
-#mkdir -p build/Debug/CUDA2-Linux-x86/_ext/2078672788
-
-#nvcc -std=c++11 --generate-code arch=compute_20,code=sm_21 -rdc=true   -c -g -I/usr/include -I/usr/local/cuda/include -o build/Debug/CUDA2-Linux-x86/_ext/2078672788/main.o /home/michele/Dropbox/PhD/NetBeansProjects/Neural_Network/main.cu
-
-#mkdir -p dist/Debug/CUDA2-Linux-x86
-#nvcc -std=c++11 --generate-code arch=compute_20,code=sm_21 -rdc=true    -o dist/Debug/CUDA2-Linux-x86/neural_network build/Debug/CUDA2-Linux-x86/_ext/2078672788/main.o -L/usr/local/cuda-8.0/lib64 -lcurand -lcublas
-
-
-
-
-#"/usr/bin/make" -f PROVA/nbproject/Makefile-Debug.mk QMAKE= SUBPROJECTS= .build-conf
-
-#"/usr/bin/make"  -f PROVA/nbproject/Makefile-Debug.mk dist/Debug/CUDA2-Linux-x86/neural_network
-
 mkdir -p PROVA/build/Debug/CUDA2-Linux-x86/_ext/2078672788
 nvcc -std=c++11 --generate-code arch=compute_20,code=sm_21 -rdc=true   -c -g -I/usr/include -I/usr/local/cuda/include -o PROVA/build/Debug/CUDA2-Linux-x86/_ext/2078672788/main.o /home/michele/Dropbox/PhD/NetBeansProjects/Neural_Network/main.cu
 
